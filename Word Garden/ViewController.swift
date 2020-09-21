c8829ea474a66823d7be96d3339bc6fd22ac2d3d
Jennifer Joseph <josephjz@bc.edu>
2020-09-20 23:34:28 -0400

    Fixed issue that _ _ _ _ and then the correctly filled in letters were not appearing - had conflicting restraints that did not allow the text field to display

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,22 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var flowerImageView: UIImageView!
     
+    @IBOutlet weak var flowerImageView: UIImageView!
     @IBOutlet weak var wordsGuessedLabel: UILabel!
     @IBOutlet weak var wordsRemainingLabel: UILabel!
     @IBOutlet weak var wordsMissedLabel: UILabel!
-    @IBOutlet weak var wordsInGame: UILabel!
-    
+    @IBOutlet weak var wordsInGameLabel: UILabel!
     @IBOutlet weak var wordBeingRevealedLabel: UILabel!
-    
     @IBOutlet weak var guessLetterButton: UIButton!
-    
     @IBOutlet weak var guessedLetterTextField: UITextField!
-    
     @IBOutlet weak var playAgainButton: UIButton!
-    
     @IBOutlet weak var gameStatusMessageLabel: UILabel!
+    
+    var wordsToGuess = ["SWIFT","DOG","CAT"]
+    var currentWordIndex = 0
+    var wordToGuess = ""
+    var lettersGuessed = ""
+    let maxNumberOfWrongGuesses = 8
+    var wrongGuessesRemaining = 8
+    var wordsGuessedCount = 0
+    var wordsMissedCount = 0
+    var guessCount = 0
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +39,8 @@ class ViewController: UIViewController {
         // so that guess a letter button is disabled when app is open
         let text = guessedLetterTextField.text!
         guessLetterButton.isEnabled = !(text.isEmpty)
+        wordToGuess = wordsToGuess[currentWordIndex]
+        wordBeingRevealedLabel.text = "_" + String(repeating: " _", count: wordToGuess.count-1)
     }
     
     func updateUIAfterGuesses() {
@@ -41,12 +50,56 @@ class ViewController: UIViewController {
         guessLetterButton.isEnabled = false
     }
     
+    func formatRevealedWord() {
+        var revealedWord = ""
+
+        // loop through all letters in wordToGuess
+        for letter in wordToGuess {
+            // check if letter in wordToGuess is in lettersGuessed (i.e. did you guess this letter already?)
+            if lettersGuessed.contains(letter) {
+                // if so, add this letter + a blank space to revealedWord
+                revealedWord = revealedWord + "\(letter) "
+            } else {
+                // if not, add an underscore + a blank space, to revealedWord
+                revealedWord = revealedWord + "_ "
+            }
+        }
+        // remove the extra space at the end of revealedWord
+        revealedWord.removeLast()
+        wordBeingRevealedLabel.text = revealedWord
+    }
+    
+    func guessALetter() {
+        // get current letter guessed and add it to all lettersGuessed
+        let currentLetterGuessed = guessedLetterTextField.text!
+        lettersGuessed = lettersGuessed + currentLetterGuessed
+        
+        //format and show revealedWord in wordBeingRevealedLabel to include new guess
+        formatRevealedWord()
+        
+        // update image, if needed, and keep track of wrong guesses
+        if !wordToGuess.contains(currentLetterGuessed) {
+            wrongGuessesRemaining = wrongGuessesRemaining - 1
+            flowerImageView.image = UIImage(named: "flower\(wrongGuessesRemaining)")
+        }
+        
+        //update game status message label
+        guessCount += 1
+        var guesses = "Guesses"
+        if guessCount == 1 {
+            guesses = "Guess"
+        }
+        gameStatusMessageLabel.text = "You've Made \(guessCount) \(guesses)"
+    }
+    
     @IBAction func doneKeyPressed(_ sender: UITextField) {
+        guessALetter()
         //this hides the keyboard
        updateUIAfterGuesses()
     }
     
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        guessALetter()
         //this hides the keyboard
        updateUIAfterGuesses()
     }
@@ -63,7 +116,7 @@ class ViewController: UIViewController {
         // have to use space " " not empty string "" because .last gives us back a character
         // sender is a constant, but the properties inside of it are mutable
         sender.text = String(sender.text?.last ?? " ").trimmingCharacters(in: .whitespaces)
-        guessLetterButton.isEnabled = !(text.isEmpty)
+        guessLetterButton.isEnabled = !(sender.text!.isEmpty)
         
 //        if let lastCharacter = text.last {
 //            guessedLetterTextField.text = String(lastCharacter)
